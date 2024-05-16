2fcac6223566c0a4ab4f976a3dfc323a1295a2ab
Tuhin810 <tuhin.thakur1233@gmail.com>
2024-05-16 12:08:39 +0530

    FEAT: otp api refactored from msg91 to ssl

--- node_server_ts/src/api/v1/controllers/auth/otp.controller.ts
+++ node_server_ts/src/api/v1/controllers/auth/otp.controller.ts
@@ -1,28 +1,127 @@
-import { Request, Response } from "express";
+// import { Request, Response } from "express";
 
+// import axios from "axios";
+// import { MESSAGE } from "../../../../constants/message";
+
+// export const getOtp = async (req: Request, res: Response) => {
+//   try {
+//     const otp = Math.floor(1000 + Math.random() * 9000).toString();
+//     const { phone_no } = req.body;
+//     //added 91 + in frontend
+//     // Prepare the request body for the external API
+//     const requestBody = {
+//       otp,
+//     };
+
+//     const response = await axios.post(
+//       `https://control.msg91.com/api/v5/otp?template_id=65477be26a3a0637307969c1&mobile=${phone_no}&authkey=409386AGL9yWS2654773b2P1`,
+//       requestBody
+//     );
+
+//     res
+//       .status(response.status)
+//       .json({ message: MESSAGE.post.succ, result: otp });
+//   } catch (error) {
+//     console.error("Error verifying OTP:", error);
+//     res.status(400).json({ message: MESSAGE.post.fail });
+//   }
+// };
+
+import { Request, Response } from "express";
+import RetailerrModel from "../../../../models/customer.model";
 import axios from "axios";
 import { MESSAGE } from "../../../../constants/message";
+import { generateOTP } from "../../../../services/generateOtp";
+
+const apiUrl = "https://smsmassdata.massdata.xyz/api/sms/send";
+const apiKey = "01866049600.ab4b8829-0375-4048-bb33-eddfb739c58e";
+const type = "text";
+const senderId = "8809612440728";
+
+const default_profile_image_url =
+  "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg";
 
 export const getOtp = async (req: Request, res: Response) => {
+  const { phone_no } = req.body;
   try {
-    const otp = Math.floor(1000 + Math.random() * 9000).toString();
-    const { phone_no } = req.body;
-
-    // Prepare the request body for the external API
-    const requestBody = {
-      otp,
+    const userInstance = await RetailerrModel.findOne({ mobile: phone_no });
+    console.log("userInstance", userInstance);
+    if (userInstance) {
+      if (userInstance.profilePhoto !== default_profile_image_url) {
+        return res.status(409).json({
+          message: MESSAGE.get.custom("User Exists"),
+        });
+      } else {
+        await RetailerrModel.deleteOne({ mobile: phone_no });
+      }
+    }
+    const otp = generateOTP();
+    const generateMessage = () => {
+      return `your OTP for login in Muslim Matrimony is ${otp} `;
     };
 
-    const response = await axios.post(
-      `https://control.msg91.com/api/v5/otp?template_id=65477be26a3a0637307969c1&mobile=${phone_no}&authkey=409386AGL9yWS2654773b2P1`,
-      requestBody
-    );
+    const dynamicMessage = generateMessage();
+    const urlWithDynamicMessage = `${apiUrl}?apiKey=${apiKey}&type=${type}&contactNumbers=${
+      "88" + phone_no
+    }&senderId=${senderId}&textBody=${encodeURIComponent(dynamicMessage)}`;
 
-    res
-      .status(response.status)
-      .json({ message: MESSAGE.post.succ, result: otp });
+    axios
+      .get(urlWithDynamicMessage)
+      .then((response: { data: any }) => {
+        console.log(response);
+        return res.status(200).json({
+          message: MESSAGE.get.succ,
+          result: otp,
+        });
+      })
+      .catch((error: { message: any }) => {
+        return res.status(400).json({
+          message: MESSAGE.get.fail,
+          result: error,
+        });
+      });
   } catch (error) {
-    console.error("Error verifying OTP:", error);
-    res.status(400).json({ message: MESSAGE.post.fail });
+    console.log(error);
+  }
+};
+
+export const forgetPasswordOtp = async (req: Request, res: Response) => {
+  const { phone_no } = req.query;
+  try {
+    const isUserExist = await RetailerrModel.findOne({ mobile: phone_no });
+    if (!isUserExist) {
+      return res.status(401).json({
+        message: MESSAGE.get.custom("User Does Not Exists"),
+      });
+    }
+    const otp = generateOTP();
+    // Function to generate a dynamic message
+    const generateMessage = () => {
+      // Your message generation logic goes here
+      return `your OTP for login in Muslim Matrimony is ${otp} `;
+    };
+
+    const dynamicMessage = generateMessage();
+    const urlWithDynamicMessage = `${apiUrl}?apiKey=${apiKey}&type=${type}&contactNumbers=${
+      "88" + phone_no
+    }&senderId=${senderId}&textBody=${encodeURIComponent(dynamicMessage)}`;
+
+    axios
+      .get(urlWithDynamicMessage)
+      .then((response: { data: any }) => {
+        console.log(response);
+        return res.status(200).json({
+          message: MESSAGE.get.succ,
+          result: otp,
+        });
+      })
+      .catch((error: { message: any }) => {
+        return res.status(400).json({
+          message: MESSAGE.get.fail,
+          result: error,
+        });
+      });
+  } catch (error) {
+    console.log(error);
   }
 };
---
+++ node_server_ts/src/services/generateOtp.ts
@@ -0,0 +1,5 @@
+export const generateOTP = (): string => {
+	// Generate a random 4-digit number and convert it to a string
+	const otp = Math.floor(1000 + Math.random() * 9000).toString();
+	return otp;
+}
