f46bfe106ad6bb236ac309a0dbb745bd9f11fc91
Abhishek Kumar <134307387+A-bhiSheKumar@users.noreply.github.com>
2024-05-25 19:20:30 +0530

    feat: Logout button + logout funtionality + setting and styles fix in drawwer

--- BazarPayWholeSaler/src/components/main/drawerContent/DrawerContent.tsx
+++ BazarPayWholeSaler/src/components/main/drawerContent/DrawerContent.tsx
@@ -9,15 +9,29 @@ import { useNavigation } from "expo-router";
 import { createDrawerItems } from "../../../database/drawerItems/drawerItems";
 import AuthContext from "../../../contexts/authContext/authContext";
 import WholesalerContext from "../../../contexts/wholesalerContext/wholesalerContext";
+import { Ionicons } from "@expo/vector-icons";
 
 export const DrawerContent = ({ onClose }: any) => {
-  const {wholesaler} = useContext(WholesalerContext);
-  const {user} = useContext(AuthContext);
+  const { wholesaler } = useContext(WholesalerContext);
+  const { user, setUser } = useContext(AuthContext);
   const navigation: any = useNavigation();
   const drawerItems = createDrawerItems(navigation);
   const handleNavigatetoProfile = ({ route }: any) => {
     navigation.navigate("profilePage");
     onClose();
+  };
+
+  const handleNavigateToSettings = () => {
+    // navigation.navigate("settingsPage");
+    // onClose();
+    console.log("Setting Clicked");
+  };
+
+  const handleLogout = () => {
+    setUser(null); // Set user to null to simulate logout
+    onClose();
+    // Optionally navigate to a login or welcome screen after logout
+    navigation.navigate("signInPage"); // Adjust the route name as needed
   };
 
   return (
@@ -33,7 +47,7 @@ export const DrawerContent = ({ onClose }: any) => {
         style={{ alignItems: "center", marginBottom: 20, flexDirection: "row" }}
       >
         <Image
-          source={{uri: wholesaler?.owner_photo}}
+          source={{ uri: wholesaler?.owner_photo }}
           style={{ width: 80, height: 80, borderRadius: 50 }}
         />
         <View style={{ marginLeft: 10 }}>
@@ -52,6 +66,7 @@ export const DrawerContent = ({ onClose }: any) => {
               paddingHorizontal: 5,
               paddingVertical: 7,
               borderRadius: 5,
+              width: 100,
             }}
           >
             <Text style={{ color: Colors.light.primary }}>Show Details</Text>
@@ -63,6 +78,45 @@ export const DrawerContent = ({ onClose }: any) => {
           <DrawerCards key={index} {...item} />
         ))}
       </View>
+
+      <TouchableOpacity
+        // onPress= // Assuming you have a logout function in AuthContext
+        onPress={handleLogout}
+        style={{
+          position: "absolute",
+          bottom: 20,
+          right: 20,
+          borderWidth: 2,
+          borderColor: Colors.light.primary,
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: 10,
+          paddingVertical: 7,
+          borderRadius: 5,
+        }}
+      >
+        <Feather
+          name="log-out"
+          size={20}
+          color={Colors.light.primary}
+          style={{ marginRight: 5 }}
+        />
+        <Text style={{ color: Colors.light.primary }}>Logout</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleNavigateToSettings}
+        style={{
+          position: "absolute",
+          bottom: 20,
+          left: 20,
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: 10,
+          paddingVertical: 7,
+        }}
+      >
+        <Ionicons name="settings" size={24} color={Colors.light.primary} />
+      </TouchableOpacity>
     </View>
   );
 };
--- BazarPayWholeSaler/src/config/config.ts
+++ BazarPayWholeSaler/src/config/config.ts
@@ -1,9 +1,9 @@
 export const headers = {
-	Accept: "application/json",
-	"Content-Type": "application/json"
+  Accept: "application/json",
+  "Content-Type": "application/json",
 };
 
-export const base_url = "http://192.168.0.102"
-export const port = '8989'
-export const version ="v1"
-export const url = base_url+':'+port
+export const base_url = "http://192.168.31.160";
+export const port = "8989";
+export const version = "v1";
+export const url = base_url + ":" + port;
