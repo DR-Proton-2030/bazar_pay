7fd817bf52e62e72a3e1b3dabdf731982907bd76
anik0810 <anikdutta0810@gmail.com>
2024-05-26 12:23:11 +0530

    chore: mongo in prod

--- node_server_ts/src/config/config.ts
+++ node_server_ts/src/config/config.ts
@@ -1,9 +1,9 @@
-export const NODE_ENV: "PROD" | "DEV" | "LOCAL" = "PROD";
+export const NODE_ENV: "PROD" | "DEV" | "LOCAL" = "LOCAL";
 
 export const MONGO_URI ={
     LOCAL: "mongodb+srv://drprotonofficial:Adarsha%40123@cluster0.9ogg6pi.mongodb.net/bazar_pay",
     DEV: "",
-    PROD: "mongodb://127.0.0.1:27017/bazar_pay"
+    PROD: "mongodb://bp_admin:Proton%402030@127.0.0.1:27017/bazar_pay?authSource=bazar_pay"
 }
 
 export const msg_apiUrl = "https://smsmassdata.massdata.xyz/api/sms/send";
