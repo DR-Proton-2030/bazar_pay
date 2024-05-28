ec8e83ccf7ad011b1a3f32a6c54ea5cb707a6a94
anik0810 <anikdutta0810@gmail.com>
2024-05-29 00:56:35 +0530

    BUILD

--- node_server_ts/dist/config/config.js
+++ node_server_ts/dist/config/config.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.msg_senderId = exports.msg_type = exports.msg_apiKey = exports.msg_apiUrl = exports.MONGO_URI = exports.NODE_ENV = void 0;
-exports.NODE_ENV = "PROD";
+exports.NODE_ENV = "LOCAL";
 exports.MONGO_URI = {
     LOCAL: "mongodb+srv://drprotonofficial:Adarsha%40123@cluster0.9ogg6pi.mongodb.net/bazar_pay",
     DEV: "",
--- node_server_ts/dist/models/customer.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const retailer_schema_1 = __importDefault(require("./schemaDefinitions/retailer.schema"));
-const RetailerrModel = (0, mongoose_1.model)("Retailer", retailer_schema_1.default);
-exports.default = RetailerrModel;
