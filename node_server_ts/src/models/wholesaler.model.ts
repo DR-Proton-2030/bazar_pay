232b1012993696138f826649a079da0ce5ee3917
coderaditya26 <coderaditya26@gmail.com>
2024-07-03 03:27:55 +0530

    refactor: update comments

--- node_server_ts/src/models/wholesaler.model.ts
+++ node_server_ts/src/models/wholesaler.model.ts
@@ -2,6 +2,7 @@ import { model } from "mongoose";
 import { IWholesaler } from "../@types/types/wholesaler.interface";
 import wholesalerSchema from "./schemaDefinitions/wholesaler.schema";
 
+// Need to Update the model
 const WholesalerModel = model<IWholesaler>("wholesalers", wholesalerSchema);
 
 export default WholesalerModel;
