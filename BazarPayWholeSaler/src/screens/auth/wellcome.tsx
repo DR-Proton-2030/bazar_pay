27b2668b960326a14e0229a524ed7d671cf8cbc8
Tuhin810 <tuhin.thakur1233@gmail.com>
2024-10-29 17:52:52 +0530

    feat: adding location request in ap

--- BazarPayWholeSaler/src/screens/auth/wellcome.tsx
+++ BazarPayWholeSaler/src/screens/auth/wellcome.tsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import BodyContent from "../../components/main/bodyContent/BodyContent";
 import { SkipBtn } from "../../components/shared/skipBtn/skipBtn";
+import * as Location from 'expo-location';
 
 export default function WellcomeScreen({ navigation }: any) {
+
+  const [location, setLocation] = useState<any>(null);
+  const [errorMsg, setErrorMsg] = useState<any>(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(location)
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  
   return (
     <ScrollView style={styles.container} contentContainerStyle={{paddingBottom:40}}>
-      {/* <SkipBtn /> */}
       <BodyContent />
     </ScrollView>
   );
